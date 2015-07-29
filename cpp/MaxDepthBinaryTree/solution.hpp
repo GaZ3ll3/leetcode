bac4b3a369c9ad93d66fc5804663d7108052bea3
Yimin <GaZ3ll3@users.noreply.github.com>
2015-07-29 11:01:24 -0500

    update forest case
    google interview

--- cpp/MaxDepthBinaryTree/solution.hpp
+++ cpp/MaxDepthBinaryTree/solution.hpp
@@ -10,3 +10,16 @@ public:
     return max(res_l,res_r);
     }
 };
+
+/* for a forest : google phone interview
+ * 
+ * core should be:
+ 
+ * int max = 0;
+ * if (!root) return 0;
+ * for (auto iter = root->children.begin(); iter != root->children.end(); iter++)
+ * {
+      max = std::max(max, maxDepth(*iter)) + 1;
+ * }
+ * return max;
+ */
